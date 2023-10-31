26e2d07b32902c75684ed5054b2b70fad712b94a
Harrzie3 <127936804+Harrzie3@users.noreply.github.com>
2023-10-31 14:18:11 +0100

    portfolio website finsish

---
+++ jvs/javascript.js
@@ -0,0 +1,22 @@
+
+
+function Dutch(){
+
+    document.getElementById("aboutMe").innerText ="Hallo! Ik ben Suus Houbraken en ik studeer software development op het SintLucas in Eindhoven. Graag wil ik game en web development doen, maar mijn voor keur gaat uit naar web development. Als hobby doe ik graag gamen en ik ben graag bezig met creatieve dingen. Ik heb gewerkt met HTML, CSS,C# en Javascript. Ik een geweldige team werker ook kan ik goed met stress omgaan dus ook met deadlines. Je kan mijn contact informatie vinden aan de onderkant van de pagina of in de navigatie lijn hier boven.";
+    document.getElementById("clu").innerText ="We moesten een game maken voor de telefoon en ik wilde graag cluedo maken voor de telefoon. Je moet gewoon rondlopen en je zoekt voor de missende kleur en na 60 seconde krijg je een quiz. Als je de quiz faalt moet je moet je de game opnieuw spelen. Voor dit project gebruikte ik Unity en C#.";
+    document.getElementById("pon").innerText = "Ik heb nog nooit pong gezien die je met vier mensen kan doen, en dat ik precies wat ik gemaakt hebt. Het is gewoon normale pong maar dan met 4 mensen het heeft ook een werkend scoreboard. Dit is mijn eerste unity project dus alles moest ik opzoeken en uitvogelen hoe ik het moest maken en heb dus veel geleerd. Voor dit project heb ik Unity en C# gebruikt. ";
+    document.getElementById("pon").style.fontSize = "large";
+    document.getElementById("sbt").innerText ="Voor een school opdracht moesten wij iets nieuws gaan leren, en ik wist niet hoe ik een werkende volume slider kan maken. Ik heb het voor elkaar gekregen om de volgende slider te maken: Volume, Speed, effect en echo. Voor dit project heb ik c# en Unity gebruikt"
+    document.getElementById("vidBox2").innerText ="Dit is de eerste video dat ik gemaakt. Het is opgenomen met een van mijn vrienden, het is een event hosted door DippyBlether. De doel van het evenement is om time shards te verzamelen en zo snel mogelijk naar de finish gaan"
+    document.getElementById("vidBox2").style.fontSize ="x-large";
+}
+
+function English(){
+
+    document.getElementById("aboutMe").innerText ="Hello, I’m Suus Houbraken and I study software development at SintLucas in Eindhoven. I primarily want to do web or game development. As of right now, I prefer web development. For my hobbies, I like gaming and keeping myself busy with things I haven't tried before. I've worked with HTML, CSS, C# and JavaScript so far and I'm always trying to improve. A great reason to consider hiring me is: I’m a fantastic team worker and I can handle stress and/or deadlines very well. You can find my contact information at the bottom of my website (using the nav-bar above).";
+    document.getElementById("clu").innerText ="We needed to create a mobile game and I wanted to make Cluedo into a mobile format.You basically need to walk around and look for the missing color, and after 60 seconds you get a quiz. If you fail that quiz you need to start the game over again. For this project I used Unity and C#.";
+    document.getElementById("pon").innerText = "I haven't seen Pong with 4 players wanted to make it, so that’s exactly what I did." + "It's just normal Pong, but with 4 players and a working scoreboard. This is also my first Unity project so everything included I either needed to look up or learn how to make it. For this project I used Unity and C#. ";
+    document.getElementById("sbt").innerText ="For a school assignment we needed to learn something new, and I didn't know how to add a working volume slider so I tackled that. I managed to make a button with an adjustable volume, speed, effect and echo slider. For this project I used Unity and C#."
+    document.getElementById("vidBox2").innerText ="This is the first video I made. It's recorded with a friend of mine, in an event hosted by DippyBlether. The event is centered around collecting time shards and racing to the finish."
+    document.getElementById("vidBox2").style.fontSize ="large";
+}
